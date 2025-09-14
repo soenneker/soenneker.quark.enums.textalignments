3dd2f70cf12c800451184a7e5d9a878d366907e3
Jake Soenneker <jake@soenneker.com>
2025-09-14 14:25:30 -0500

    Initial commit

---
+++ src/TextAlignment.cs
@@ -0,0 +1,18 @@
+﻿using Intellenum;
+
+namespace Soenneker.Quark.Enums.TextAlignments;
+
+/// <summary>
+/// Common html text alignments in enumeration form.
+/// </summary>
+[Intellenum<string>]
+public sealed partial class TextAlignment
+{
+    public static readonly TextAlignment Left = new("left");
+    public static readonly TextAlignment Right = new("right");
+    public static readonly TextAlignment Center = new("center");
+    public static readonly TextAlignment Justify = new("justify");
+    public static readonly TextAlignment Start = new("start");
+    public static readonly TextAlignment End = new("end");
+    public static readonly TextAlignment MatchParent = new("match-parent");
+}
---
+++ test/Soenneker.Quark.Enums.TextAlignments.Tests/TextAlignmentTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Quark.Enums.TextAlignments.Tests;
+
+public sealed class TextAlignmentTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
