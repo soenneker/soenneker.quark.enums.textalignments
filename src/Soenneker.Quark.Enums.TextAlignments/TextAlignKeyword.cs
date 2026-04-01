49abc099dfc60ac0d4936e6a26fc26695e823430
Jake Soenneker <jake@soenneker.com>
2026-04-01 13:35:15 -0500

    -> tailwind

--- src/Soenneker.Quark.Enums.TextAlignments/TextAlignKeyword.cs
+++ src/Soenneker.Quark.Enums.TextAlignments/TextAlignKeyword.cs
@@ -1,19 +1,15 @@
 using Soenneker.Gen.EnumValues;
 
-namespace Soenneker.Quark.Enums;
+namespace Soenneker.Quark;
 
 /// <summary>
 /// Common html text alignments in enumeration form.
 /// </summary>
 [EnumValue<string>]
-[IncludeEnumValues(typeof(GlobalKeyword))]
 public sealed partial class TextAlignKeyword
 {
     public static readonly TextAlignKeyword Left = new("left");
     public static readonly TextAlignKeyword Right = new("right");
     public static readonly TextAlignKeyword Center = new("center");
     public static readonly TextAlignKeyword Justify = new("justify");
-    public static readonly TextAlignKeyword Start = new("start");
-    public static readonly TextAlignKeyword End = new("end");
-    public static readonly TextAlignKeyword MatchParent = new("match-parent");
 }
