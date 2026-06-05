3fb1046bf1895e1c98a5bdecca8e3a4453ab9f7e
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:16 -0500

    xml commenting

--- src/Soenneker.Quark.Enums.TextAlignments/TextAlignmentEnum.cs
+++ src/Soenneker.Quark.Enums.TextAlignments/TextAlignmentEnum.cs
@@ -2,13 +2,34 @@ using Soenneker.Gen.EnumValues;
 
 namespace Soenneker.Quark;
 
+/// <summary>
+/// Represents the text alignment enum.
+/// </summary>
 [EnumValue<string>]
 public sealed partial class TextAlignmentEnum
 {
+    /// <summary>
+    /// The left.
+    /// </summary>
     public static readonly TextAlignmentEnum Left = new("text-left");
+    /// <summary>
+    /// The start.
+    /// </summary>
     public static readonly TextAlignmentEnum Start = new("text-start");
+    /// <summary>
+    /// The center.
+    /// </summary>
     public static readonly TextAlignmentEnum Center = new("text-center");
+    /// <summary>
+    /// The end.
+    /// </summary>
     public static readonly TextAlignmentEnum End = new("text-end");
+    /// <summary>
+    /// The right.
+    /// </summary>
     public static readonly TextAlignmentEnum Right = new("text-right");
+    /// <summary>
+    /// The justify.
+    /// </summary>
     public static readonly TextAlignmentEnum Justify = new("text-justify");
 }
